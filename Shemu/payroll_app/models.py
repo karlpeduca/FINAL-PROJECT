9f9ebcab4e8b3573b039fbf11a8e1efdf57f740c
HifumiKaede <ericjudo2@gmail.com>
2026-04-21 20:01:17 +0800

    Fixed backend stuff

--- Shemu/payroll_app/models.py
+++ Shemu/payroll_app/models.py
@@ -1,35 +1,35 @@
 from django.db import models
-from django.utils import timezone
-# Create your models here.
+
 
 class Employee(models.Model):
     name = models.CharField(max_length=300)
     id_number = models.CharField(max_length=50, primary_key=True, unique=True)
     rate = models.FloatField()
-    overtime_pay = models.FloatField(null=True,blank=True)
-    allowance = models.FloatField(null=True,blank=True)
+    overtime_pay = models.FloatField(null=True, blank=True)
+    allowance = models.FloatField(null=True, blank=True)
 
     def getName(self):
         return self.name
-    
+
     def getID(self):
         return self.id_number
-    
+
     def getRate(self):
         return self.rate
-    
+
     def getOvertime(self):
         return self.overtime_pay
-    
+
     def resetOvertime(self):
         self.overtime_pay = 0
         self.save()
-    
+
     def getAllowance(self):
         return self.allowance
-    
+
     def __str__(self):
-        return f"pk:{self.id_number}, rate:{self.rate}" #UNSURE
+        return f"pk:{self.id_number}, rate:{self.rate}"
+
 
 class Payslip(models.Model):
     id_number = models.ForeignKey(Employee, on_delete=models.CASCADE)
@@ -48,46 +48,52 @@ class Payslip(models.Model):
     total_pay = models.FloatField()
 
     def getIDNumber(self):
-        return self.employee.id_number
-    
+        return self.id_number.id_number
+
     def getMonth(self):
         return self.month
-    
-    def getDate(self):
+
+    def getDate_range(self):
         return self.date_range
-    
+
     def getYear(self):
         return self.year
-    
+
     def getPay_cycle(self):
         return self.pay_cycle
-    
+
     def getRate(self):
         return self.rate
-    
+
     def getCycleRate(self):
         return self.rate / 2
-    
+
     def getEarnings_allowance(self):
         return self.earnings_allowance
-    
+
     def getDeductions_tax(self):
         return self.deductions_tax
-    
+
     def getDeductions_health(self):
         return self.deductions_health
-    
+
     def getPag_ibig(self):
         return self.pag_ibig
-    
+
     def getSSS(self):
         return self.sss
-    
+
     def getOvertime(self):
         return self.overtime
-    
+
     def getTotal_pay(self):
         return self.total_pay
-    
+
     def __str__(self):
-        return f"pk: {self.pk}, Employee: {self.employee.id_number}, Period: {self.month, self.date_range}, {self.year}, Cycle: {self.pay_cycle}, Total Pay: {self.total_pay}"
+        return (
+            f"pk: {self.pk}, "
+            f"Employee: {self.id_number.id_number}, "
+            f"Period: {self.month} {self.date_range}, {self.year}, "
+            f"Cycle: {self.pay_cycle}, "
+            f"Total Pay: {self.total_pay}"
+        )
--- Shemu/payroll_app/views.py
+++ Shemu/payroll_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Employee, Payslip
 
-# Create your views here.
-def base(request):
-    return render(request,'payroll_app/base.html')
+def employees(request):
+    all_employees = Employee.objects.all()
+    return render(request, 'payroll_app/employees.html', {'employees': all_employees})
