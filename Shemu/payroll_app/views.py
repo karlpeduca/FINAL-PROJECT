7eafe032e437b9b86b8abfb0d20636d378c37f7a
karlpeduca <karl.nicholai.peduca@student.ateneo.edu>
2026-04-21 14:18:01 +0800

    someone pls do the front end

--- Shemu/payroll_app/views.py
+++ Shemu/payroll_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Employee, Payslip
 
 # Create your views here.
+def base(request):
+    return render(request,'payroll_app/base.html')
